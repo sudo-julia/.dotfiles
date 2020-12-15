6b3f8032fb44a2d649e36bb89d4b30f1a04858af
sudo-julia <jlearning@tuta.io>
2020-12-15 10:54:42 -0800

    todo-checks updates at lastest modification

--- config/polybar/scripts/todo-checks.sh
+++ config/polybar/scripts/todo-checks.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/bash
 
-# TODO integrate `stat -c "&Y"` to check if file has been modified
-#      to prevent all these processes every second lmao
+newestMod=$( stat -c "%Y" ~/.TODO.txt )
+lastEdit=$( cat ~/.TODO.last )
 
-WORKING=$( grep -cs '^!' "$HOME"/.TODO.txt )
-TODO=$( grep -cs '^*\|^!' "$HOME"/.TODO.txt )
-DONE=$( grep -cs '^X' "$HOME"/.TODO.txt )
-
-if (( WORKING != 0 )); then
-	if (( TODO > 10 )); then
-		echo "! $WORKING  * $TODO  X $DONE ~"
+updateModule () {
+	echo "$newestMod" > ~/.TODO.last
+	WORKING=$( grep -cs '^!' "$HOME"/.TODO.txt )
+	TODO=$( grep -cs '^*\|^!' "$HOME"/.TODO.txt )
+	DONE=$( grep -cs '^X' "$HOME"/.TODO.txt )
+	if (( WORKING != 0 )); then
+		if (( TODO > 10 )); then
+			echo "! $WORKING  * $TODO  X $DONE ~"
+		else
+			EXTRA=$( grep -cs '^/\|^~' "$HOME"/.TODO.txt )
+			echo "! $WORKING  % $EXTRA  * $TODO  X $DONE ~"
+		fi
 	else
-		EXTRA=$( grep -cs '^/\|^~' "$HOME"/.TODO.txt )
-		echo "! $WORKING  % $EXTRA  * $TODO  X $DONE ~"
+		if (( TODO > 10 )); then
+			echo "* $TODO  X $DONE ~"
+		else
+			EXTRA=$( grep -cs '^/\|^~' "$HOME"/.TODO.txt )
+			echo "% $EXTRA  * $TODO  X $DONE ~"
+		fi
 	fi
+}
+
+if [[ "$newestMod" -gt "$lastEdit" ]]; then
+	updateModule
 else
-	if (( TODO > 10 )); then
-		echo "* $TODO  X $DONE ~"
-	else
-		EXTRA=$( grep -cs '^/\|^~' "$HOME"/.TODO.txt )
-		echo "% $EXTRA  * $TODO  X $DONE ~"
-	fi
+	exit 0
 fi
