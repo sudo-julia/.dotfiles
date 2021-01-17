62ae5c21b47827919deda69c74d3109a1a007b0f
sudo-julia <jlearning@tuta.io>
2021-01-17 11:19:53 -0800

    little bit of this little bit of that

--- config/polybar/scripts/todo-checks.sh
+++ config/polybar/scripts/todo-checks.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/bash
 # puts number of items in markdown todo list in polybar
 
-newestMod=$( stat -c "%Y" ~/.TODO.md )
-lastMod=$( cat ~/.TODO.last )
+TODO_FILE="${HOME}/lib/TODO.md"
+newestMod="$( stat -c '%Y' "${TODO_FILE}" )"
+lastMod="$( cat "${HOME}/lib/TODO.last" )"
 
-updateModule () {
-	echo "$newestMod" > ~/.TODO.last
-	WORKING=$( grep -cs -- '<!--a-->$' "${HOME}"/.TODO.md )
-	TODO=$( grep -cs -- '^- \[ \]' "${HOME}"/.TODO.md )
-	DONE=$( grep -cs -- '^- \[X\]' "${HOME}"/.TODO.md )
+updateModule() {
+	echo "${newestMod}" > "${HOME}/lib/TODO.last"
+	WORKING="$( grep -cs -- '<!--a-->$' "${TODO_FILE}" )"
+	TODO="$( grep -cs -- '^- \[ \]' "${TODO_FILE}" )"
+	DONE="$( grep -cs -- '^- \[X\]' "${TODO_FILE}" )"
 	if (( WORKING != 0 )); then
 		if (( TODO > 10 )); then
-			echo "! $WORKING  * $TODO  X $DONE ~"
+			echo "! ${WORKING}  * ${TODO}  X ${DONE} ~"
 		else
-			EXTRA=$( grep -cs '^/\|^~' "$HOME"/.TODO.md )
-			echo "! $WORKING  % $EXTRA  * $TODO  X $DONE ~"
+			EXTRA="$( grep -cs '^/\|^~' "${TODO_FILE}" )"
+			echo "! ${WORKING}  % ${EXTRA}  * ${TODO}  X ${DONE} ~"
 		fi
 	else
 		if (( TODO > 10 )); then
-			echo "* $TODO  X $DONE ~"
+			echo "* ${TODO}  X ${DONE} ~"
 		else
-			EXTRA=$( grep -cs '^/\|^~' "$HOME"/.TODO.md )
-			echo "% $EXTRA  * $TODO  X $DONE ~"
+			EXTRA="$( grep -cs '^/\|^~' "${TODO_FILE}" )"
+			echo "% ${EXTRA}  * ${TODO}  X ${DONE} ~"
 		fi
 	fi
 }
 
-if [[ "$newestMod" -gt "$lastMod" ]]; then
+if (( newestMod > lastMod )); then
 	updateModule
 elif [[ ! "$TODO_STARTED" ]]; then
 	export TODO_STARTED=1
